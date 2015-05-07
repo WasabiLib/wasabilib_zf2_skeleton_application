fc32023c08020131f06701050887ae42d0080cae
saschaq <saschaq@users.noreply.github.com>
2015-05-07 12:43:47 +0200

    Update application.config.php

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,7 @@ return array(
         'Application',
         'WasabiLib',
         'WasabiMail',
+        'ReadMe',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
